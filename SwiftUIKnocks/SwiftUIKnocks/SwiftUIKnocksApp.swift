15d3f3725e25d8190a1e387191b556d6b234cee7
GaneshRajuGalla <ganeshrajugalla@gmail.com>
2024-01-24 19:10:03 +0530

    Project setup

---
+++ SwiftUIKnocks/SwiftUIKnocks/SwiftUIKnocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKnocksApp.swift
+//  SwiftUIKnocks
+//
+//  Created by Ganesh Raju Galla on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKnocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
